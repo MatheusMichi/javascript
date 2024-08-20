1d52ae7b449c9eed94b100ac029aff7633cbc3b7
Matheus Michi <121824923+MatheusMichi@users.noreply.github.com>
2024-08-20 14:14:02 -0300

    criação dos ex008, 009 e 010

---
+++ exercícios/ex008/index.js
@@ -0,0 +1,6 @@
+var vel = 61.9
+console.log(`Você está a ${vel}Km/h`)
+console.log('Use sempre cinto de segurança!')
+if (vel > 60) {
+    console.log('Você está MULTADO  por excesso de velocidade')
+}
