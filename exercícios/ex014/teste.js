6b3b782dbb1fbd06007c91a204ff1a190922acc9
Matheus Michi <121824923+MatheusMichi@users.noreply.github.com>
2024-08-28 14:34:50 -0300

    criando ex 014 e 015

---
+++ exercícios/ex014/teste.js
@@ -0,0 +1,11 @@
+var c = 1
+
+while (c <= 10) {
+    console.log(`número: ${c}`)
+    c++
+} 
+
+do {
+    console.log(`juros ${c} subindo`)
+    c = c + 200
+} while (c <= 2000)
---
+++ exercícios/ex015/teste.js
@@ -0,0 +1,3 @@
+for (var c = 5 ; c <= 50 ; c = c + 5) {
+    console.log(c)
+}
