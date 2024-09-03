4ca05c8e40ec355cf96f6b6c7fe54991c79256cf
Matheus Michi <121824923+MatheusMichi@users.noreply.github.com>
2024-09-03 15:31:05 -0300

    finalizando o desafio 05

--- desafios/desafio 05/script.js
+++ desafios/desafio 05/script.js
@@ -21,13 +21,48 @@ function inLista(n, l) {
 
 function adicionar() {
     if (isNumero(num.value) && !inLista(num.value, valores)) {
-        window.alert('ok')
+        valores.push(Number(num.value))
+        var item = document.createElement('option')
+        item.text = `valor ${num.value} adicionado`
+        lista.appendChild(item)
+        res.innerHTML = ''
+        
     } else {
         window.alert('valor inválido ou já encontrado na lista')
     }
+
+    num.value = ''
+    num.focus()
                     
 }
 
-        
+function finalizar() {
+    if (valores.length == 0) {
+        window.alert('Não foi possível finalizar pois não há valores')
+    } else {
+        var tot = valores.length
+        var maior = valores[0]
+        var menor = valores[0]
+        var soma = 0
+        var media = 0
+
+        for (var pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior)
+                maior = valores[pos]
+            if ( valores[pos] < menor)
+                menor = valores[pos]
+            }
+            media = soma / tot
+        }
+        res.innerHTML = ''
+        res.innerHTML += `<p> Ao todo, temos ${tot} números cadastrados`
+        res.innerHTML += `<p> O maior número informado é ${maior} </p>`
+        res.innerHTML += `<p> O menor número informado é ${menor} </p>`
+        res.innerHTML += `<p> A soma de todos os valores é ${soma} </p>`
+        res.innerHTML += `<p> A média de todos os valores é ${media} </p>`
+    }
+    
+
 
 
